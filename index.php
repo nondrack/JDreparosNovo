168224f49ef68e1084b43b95effd394ed2ca8daf
nondrack <luckasfernando500@gmail.com>
2025-06-11 10:33:47 -0300

    jd reparos

---
+++ index.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/css/bootstrap.min.css" rel="stylesheet" 
+    integrity="sha384-4Q6Gf2aSP4eDXB8Miphtr37CMZZQ5oXLH2yaXMJ2w8e2ZtHTl7GptT4jmndRuHDT" crossorigin="anonymous">
+    <link rel = "stylesheet" href="css/style.css">
+    <title>JD reparos</title>
+    </head>
+<body>
+    <header class="menu-header">
+        <nav class="navbar navbar-expand-lg bg-body-tertiary">
+  <div class="container-fluid">
+    <a> <img src="image/logo.png" class="img-logo" alt=""></a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav m-auto m-2 mb-lg-0 ">
+        <li class="nav-item">
+          <a class="nav-link " href="#">Home</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="index.php">Sobre</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="index.php">Serviços</a>
+        </li>
+        </li>
+      </ul>
+      <a href="index.php" class="btn btn-danger">
+        Contato
+    </a>
+    </div>
+  </div>
+</nav>
+    </header>
+    <main>
+      <div class="banner">
+        <img src="image/banner.jpg" class="img-banner" alt="JD reparos">
+        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quasi impedit totam deleniti laboriosam 
+          non explicabo! Quo architecto doloremque deserunt nostrum aperiam earum incidunt, sapiente, nisi 
+          recusandae, nesciunt quos consequuntur tenetur.</p>
+      </div>
+    </main>
+    
+</body>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/js/bootstrap.bundle.min.js" integrity="sha384-j1CDi7MgGQ12Z7Qab0qlWQ/Qqz24Gc6BM0thvEMVjHnfYGF0rmFCozFSxQBxwHKO" crossorigin="anonymous"></script>
+</html>
